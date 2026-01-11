d57d84ebeb9e269b5484e5bc7c770869403f59cb
Aashish Singh <aashishsingh.aec@gmail.com>
2026-01-11 14:28:35 +0530

    Update context and recent changes

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -609,7 +609,6 @@ export default function AppProvider({
         handleDeleteEvent,
         handleCreateSubscription,
         handleDeleteSubscription,
-        handleDeleteSubscription,
         handleUpdateResourceCategories,
         handleUpdateDepartments,
         handleUpdateEventTypes,
